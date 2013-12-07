10ada316035c3188c48ce071fe96c0f3a875775e
chaseoes <chase.oes@gmail.com>
2013-12-07 14:50:46 -0700

    Fix join sign issue.

--- src/main/java/com/chaseoes/mobninja/listeners/PlayerInteractListener.java
+++ src/main/java/com/chaseoes/mobninja/listeners/PlayerInteractListener.java
@@ -8,15 +8,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerInteractListener implements Listener {
-    
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.hasBlock() && (event.getClickedBlock().getType() == Material.SIGN || event.getClickedBlock().getType() == Material.WALL_SIGN)) {
-            Sign s = (Sign) event.getClickedBlock();
-            if (ChatColor.stripColor(s.getLine(0)).equalsIgnoreCase("[MobNinja]")) {
-                String map = s.getLine(2);
-                event.getPlayer().performCommand("mobninja join " + map);
+        try {
+            if (event.hasBlock()) {
+                if ((event.getClickedBlock().getType() == Material.SIGN || event.getClickedBlock().getType() == Material.WALL_SIGN) && !event.getPlayer().isSneaking()) {
+                    Sign s = (Sign) event.getClickedBlock().getState();
+                    if (ChatColor.stripColor(s.getLine(0)).equalsIgnoreCase("[MobNinja]")) {
+                        String map = s.getLine(2);
+                        event.getPlayer().performCommand("mobninja join " + map);
+                    }
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
