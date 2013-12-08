a90722d8b46d1eb5d9a0a347a033eb600dfd6546
chaseoes <chase.oes@gmail.com>
2013-12-07 18:08:13 -0700

    Fix location serialization error.

---
+++ src/main/java/com/chaseoes/mobninja/utilities/PlayerState.java
@@ -0,0 +1,11 @@
+package com.chaseoes.mobninja.utilities;
+
+import org.bukkit.entity.Player;
+
+public class PlayerState {
+    
+    public PlayerState(Player player) {
+        
+    }
+
+}
--- src/main/java/com/chaseoes/mobninja/utilities/SerializableLocation.java
+++ src/main/java/com/chaseoes/mobninja/utilities/SerializableLocation.java
@@ -18,7 +18,7 @@ public class SerializableLocation {
     
     public static Location unSerializeLocation(String string) {
         String[] coords = string.split(":");
-        return new Location(MobNinja.getInstance().getServer().getWorld(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2]), Integer.parseInt(coords[3]), Float.parseFloat(coords[4]), Float.parseFloat(coords[5]));
+        return new Location(MobNinja.getInstance().getServer().getWorld(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2]), Integer.parseInt(coords[3]), Float.parseFloat(coords[5]), Float.parseFloat(coords[4]));
     }
 
 }
